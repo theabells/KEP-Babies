138e22a4b264d5e7411982b35a0f861d489bd1f8
Thea Abigail Lomibao <taylomibao@gmail.com>
2014-02-28 16:43:43 +0800

    Update edit_book_details.php

--- edit_book_details.php
+++ edit_book_details.php
@@ -1,107 +1,163 @@
 <!DOCTYPE html>
 <html>
-  <head>
-      <title>Update</title>
-  </head>
-  <body>
+<head>
+    <title>Update</title>
+</head>
+<body>
 
-    <div id="#myModal">
-      <form class="form-horizontal" role="form" method="post" action="http://localhost/cmsc128/index.php/site/update_book_database">
+<div id="#myModal">
+    <form name="material_form" class="form-horizontal" role="form" method="post" action="<?php echo base_url();?>index.php/site/update_material_details">
         <input name="accession_number" type="hidden" class="form-control" value="<?php foreach($results as $row){echo $row->accession_number;} ?>">
-          <div class="form-group">
+        <?php print_r($results)?>
+        <div class="form-group">
             <label for="inputTitle" class="col-sm-2 control-label">Title</label>
-            <div class="col-sm-10">
-              <input type="text" class="form-control" id="inputTitle" name="inputTitle" value="<?php foreach($results as $row){echo $row->title;} ?>">
+            <div class="col-sm-8">
+                <input type="text" class="form-control" id="inputTitle" name="inputTitle" value="<?php foreach($results as $row){echo $row->title;} ?>">
+                <span class="prompt" name="title_prompt"></span>
             </div>
-          </div>
-          <div class="form-group">
+        </div>
+        <div class="form-group">
             <label for="inputType" class="col-sm-2 control-label">Type</label>
             <div class="row" id="inputType">
-              <div class="col-lg-6">
-                <div class="input-group">
-                  <label for="book">Book</label>
-                  <input type="radio" name="type" value="book" id="book">
-                </div><!-- /input-group -->
-              </div><!-- /.col-lg-6 -->
-              <div class="col-lg-6">
-                <div class="input-group">
-                  <label for="thesis">Thesis</label>
-                  <input type="radio" name="type" value="thesis" id="thesis">
-                </div><!-- /input-group -->
-              </div><!-- /.col-lg-6 -->
-              <div class="col-lg-6">
-                <div class="input-group">
-                  <label for="sp">Special Project</label>
-                  <input type="radio" name="type" value="sp" id="sp">
-                </div><!-- /input-group -->
-              </div><!-- /.col-lg-6 -->
-              <div class="col-lg-6">
-                <div class="input-group">
-                  <label for="journal">Journal</label>
-                  <input type="radio" name="type" value="journal" id="journal">
-                </div><!-- /input-group -->
-              </div><!-- /.col-lg-6 -->
+                <div class="col-lg-2">
+                    <div class="input-group">
+                        <input type="radio" name="type" value="book" id="book">
+                        <label for="book">Book</label>
+                    </div><!-- /input-group -->
+                </div><!-- /.col-lg-8 -->
+                <div class="col-lg-2">
+                    <div class="input-group">
+                        <input type="radio" name="type" value="thesis" id="thesis">
+                        <label for="thesis">Thesis</label>
+                    </div><!-- /input-group -->
+                </div><!-- /.col-lg-8 -->
+                <div class="col-lg-2">
+                    <div class="input-group">
+                        <input type="radio" name="type" value="sp" id="sp">
+                        <label for="sp">Special Project</label>
+                    </div><!-- /input-group -->
+                </div><!-- /.col-lg-8 -->
+                <div class="col-lg-2">
+                    <div class="input-group">
+                        <input type="radio" name="type" value="journal" id="journal">
+                        <label for="journal">Journal</label>
+                    </div><!-- /input-group -->
+                </div><!-- /.col-lg-8 -->
             </div><!-- /.row -->
-          </div>
-          <div class="form-group">
+        </div>
+        <div class="form-group">
             <label for="inputPublisher" class="col-sm-2 control-label">Publisher</label>
-            <div class="col-sm-10">
-              <input type="text" class="form-control" id="inputPublisher" name="inputPublisher" value="<?php foreach($results as $row){echo $row->publisher;} ?>">
+            <div class="col-sm-8">
+                <input type="text" class="form-control" id="inputPublisher" name="inputPublisher" value="<?php foreach($results as $row){echo $row->publisher;} ?>">
+                <span class="prompt" name="publisher_prompt"></span>
             </div>
-          </div>
-          <div class="form-group">
+        </div>
+        <div class="form-group">
             <label for="inputYear" class="col-sm-2 control-label">Year</label>
-              <div class="col-sm-10">
-                <input type="text" class="form-control" id="inputYear" name="inputYear" value="<?php foreach($results as $row){echo $row->copyright_year;} ?>">
-              </div>
-          </div>
-          <div class="col-lg-10">
+            <div class="col-sm-2">
+                <input type="number" class="form-control" id="inputYear" name="inputYear" value="<?php foreach($results as $row){echo $row->copyright_year;} ?>" min="1900">
+            </div>
+        </div>
+        <div class="form-group">
+            <label for="inputSubject" class="col-sm-2 control-label">Subject</label>
+            <div class="col-sm-8">
+                <input type="text" class="form-control" id="inputSubject" name="inputSubject" value="<?php foreach($results as $row){echo $row->subject;} ?>">
+            </div>
+        </div>
+        
+        <div class="col-lg-8">
             <table id="author_table" class="table table-striped table-bordered table-hover">
-              <thead>
+                <thead>
                 <tr class="success">
-                  <th>Author</th>
+                    <th>Author</th>
                 </tr>
-              </thead>
-              <tbody>
+                </thead>
+                <tbody>
                 <?php foreach($results2 as $row): ?>
-                <tr>
-                  <td><input type="text" name="inputAuthor[]" value="<?php echo $row->author?>"/></td>
-                </tr>
-                <?php endforeach; ?> 
-              </tbody>
+                    <tr>
+                        <td><input type="text" name="inputAuthor[]" value="<?php echo $row->author?>"/></td>
+                    </tr>
+                <?php endforeach; ?>
+                </tbody>
             </table>
-            <input class="add_more" type="button" value="Add More" name="add_more" />
-          </div>
+            <input class="add_more" type="button" value="Add More Author" name="add_more" />
+        </div>
         <button type="submit" value="<?php foreach($results as $row){echo $row->accession_number;}?>" class="editButton btn btn-primary">Save Changes</button>
-      </form>
-    </div>
+    </form>
+</div>
 
-    <script src="<?php echo base_url();?>/js/jquery-1.9.1.js"></script>
-    <script src="<?php echo base_url();?>/js/jquery-1.9.1.min.js"></script>
-    <script src="<?php echo base_url();?>/js/main.js"></script>
+<script src="<?php echo base_url();?>/js/jquery-1.9.1.js"></script>
+<script src="<?php echo base_url();?>/js/jquery-1.9.1.min.js"></script>
+<script src="<?php echo base_url();?>/js/main.js"></script>
+<!--Validation from add material-->
+<script type="text/javascript">
+    window.onload = function(){
+      //material_form.subject.onblur = validate_subject;
+      material_form.inputTitle.onblur = validate_title;
+      material_form.inputAuthor.onblur = validate_author;
+      material_form.inputPublisher.onblur = validate_publisher;
+    }
 
-    <script type="text/javascript">
+    function validate_subject(){
+      var prompt = "";
+      str = material_form.subject.value;
+      if(str == "")
+        prompt = "This is a required field.";
+      else if(!str.match(/^[a-zA-Z][0-9a-zA-Z]+$/))
+        prompt = "Only Letters and Numbers are allowed in this field.";
+      document.getElementsByName('subject_prompt')[0].innerHTML = prompt;
+      if(prompt == "")
+        return true;
+    }
+
+    function validate_title(){
+      var prompt = "";
+      str = material_form.inputTitle.value;
+      if(str == "")
+        prompt = "This is a required field.";
+      else if(!str.match(/^[a-zA-Z][0-9a-zA-Z\-\ \.]+$/))
+        prompt = "Only Letters, Numbers, Spaces, Period and Hypen are allowed in this field.";
+      document.getElementsByName('title_prompt')[0].innerHTML = prompt;
+      if(prompt == "")
+        return true;
+    }
+
+    /*function validate_author(){
+      var prompt = "";
+      str = material_form.inputAuthor.value;
+      if(str == "")
+        prompt = "This is a required field.";
+      else if(!str.match(/^[a-zA-Z][0-9a-zA-Z\-\ \.]+$/))
+        prompt = "Only Letters, Numbers, Spaces, Period and Hypen are allowed in this field.";
+      document.getElementsByName('author_prompt')[0].innerHTML = prompt;
+      if(prompt == "")
+        return true;
+    }*/
+
+    function validate_publisher(){
+      var prompt = "";
+      str = material_form.inputPublisher.value;
+      if(str == "")
+        prompt = "This is a required field.";
+      else if(!str.match(/^[a-zA-Z][0-9a-zA-Z\-\ \.]+$/))
+        prompt = "Only Letters, Numbers, Spaces, Period and Hypen are allowed in this field.";
+      document.getElementsByName('publisher_prompt')[0].innerHTML = prompt;
+      if(prompt == "")
+        return true;
+    }
+
+</script>
+<script type="text/javascript">
     var str="<?php foreach($results as $row){echo $row->type;}?>";
-      if (str=="book") {
-        document.getElementById("book").checked=true;
-      }
-      else if(str=="thesis"){
-        document.getElementById("thesis").checked=true;
-      }
-      else if(str=="sp"){
-        document.getElementById("sp").checked=true;
-      }
-      else{
-        document.getElementById("journal").checked=true;
-      }
-      $(function () {
-          $('input.add_more').on('click', function () {
-              var $table = $('#author_table');
-              var $tr = $table.find('tr').eq(1).clone();
-              $tr.appendTo($table).find('input').val('');
-          });
-      });
-    </script>
-    <!--<link href="<?php echo base_url();?>css/bootstrap.css" rel="stylesheet">-->
-  </body>
+    document.getElementById("<?php foreach($results as $row){echo $row->type;}?>").checked=true;
+    $(function () {
+        $('input.add_more').on('click', function () {
+            var $table = $('#author_table');
+            var $tr = $table.find('tr').eq(1).clone();
+            $tr.appendTo($table).find('input').val('');
+        });
+    });
+</script>
+<link href="<?php echo base_url();?>css/bootstrap.css" rel="stylesheet">
+</body>
 </html>
